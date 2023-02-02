566b6720c5e1f77fc68bc896dfa4675ecd68267b
Yavuz ilhan <yavuzilhan@gmail.com>
2023-02-02 13:27:19 +0300

    navbar reconfig

--- fullstack-portfolio/frontend-react/src/container/Header/Header.jsx
+++ fullstack-portfolio/frontend-react/src/container/Header/Header.jsx
@@ -4,9 +4,20 @@ import { motion } from "framer-motion";
 import { images } from "../../constants";
 import "./Header.scss";
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -14,10 +25,12 @@ const Header = () => {
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
-              <p className="p-text">Hello, I am</p>
-              <h1 className="head-text">Micael</h1>
+            {/* <span>👋</span> */}
+            <div>
+              <p className="p-text">Merhaba, ben</p>
+              <h1 style={{ marginLeft: 10 }} className="head-text">
+                Yavuz ilhan
+              </h1>
             </div>
           </div>
           <div className="tag-cmp app__flex">
@@ -39,10 +52,20 @@ const Header = () => {
           src={images.circle}
           alt="profile_circle"
           className="overlay_circle"
-        ></motion.img>
+        />
       </motion.div>
 
-      <motion.div></motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
     </div>
   );
 };
