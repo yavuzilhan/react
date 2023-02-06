916d58b15a6513b5d2c7f22453ac9e0279fe20ff
Yavuz ilhan <yavuzilhan@gmail.com>
2023-02-06 19:48:38 +0300

    .env

--- fullstack-portfolio/frontend-react/src/client.js
+++ fullstack-portfolio/frontend-react/src/client.js
@@ -2,9 +2,13 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "usbxhzwa",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
-  apiVersion: "",
+  apiVersion: "2023-02-05",
   useCdn: true,
-  token: "",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
